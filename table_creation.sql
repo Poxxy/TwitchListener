509195680e10cf358cfcae5774dd467dd0047866
Poxxy <41162160+Poxxy@users.noreply.github.com>
2021-08-27 14:55:34 -0400

    Example setup for table creation

---
+++ table_creation.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Users (
+	[UserID] INT IDENTITY(1,1) PRIMARY KEY,
+	[Username] VARCHAR(32) NOT NULL
+
+)
+
+CREATE TABLE Channels (
+	[ChannelID] INT IDENTITY(1,1) PRIMARY KEY,
+	[Channel] VARCHAR(32) NOT NULL
+)
+
+CREATE TABLE Content (
+	[Channel] VARCHAR(32) NOT NULL,
+	[Username] VARCHAR(32) NOT NULL,
+	[Message] VARCHAR(32) NOT NULL,
+)
