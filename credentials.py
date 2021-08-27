4c15fef08e42267486c3bfd47c9f73f75efacaf0
Poxxy <41162160+Poxxy@users.noreply.github.com>
2021-08-27 14:50:42 -0400

    Create credentials.py
    This is an example credentials file. You'll have to put in your own twitch stuff to get the bot to connect.

---
+++ credentials.py
@@ -0,0 +1,5 @@
+CHANNEL = ['list','of','channels','this','bot','uses']
+NAME = 'Your/your bot name'
+TWITCH_TOKEN = 'oath:yourtokenhere'
+TWITCH_CLIENT_ID = 'your twitch client id'
+TWITCH_CLIENT_SECRET = 'your twitch client secret'
