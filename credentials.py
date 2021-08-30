c94029cfb41ff01c0aec106b144b3053e447dce2
Poxxy <41162160+Poxxy@users.noreply.github.com>
2021-08-30 10:53:13 -0400

    ADMIN no longer needed
    ADMIN no longer needed since chat commands have been removed.

--- credentials.py
+++ credentials.py
@@ -4,8 +4,6 @@ Name is just what you want to call the bot.
 
 twitch_token can be found at https://twitchapps.com/tmi/
 twitch_client_id and twitch_client_secret can be found at https://dev.twitch.tv/
-
-admin is a list of trusted people to use extra commands on the bot
 """
 
 CHANNEL = ['list','of','channels','this','bot','uses']
@@ -13,5 +11,3 @@ NAME = 'Your/your bot name'
 TWITCH_TOKEN = 'oath:yourtokenhere'
 TWITCH_CLIENT_ID = 'your twitch client id'
 TWITCH_CLIENT_SECRET = 'your twitch client secret'
-
-ADMIN = ['you','friend','etc']
