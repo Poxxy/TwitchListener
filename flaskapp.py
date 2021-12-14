7bac4f3b5178c453cc718a862f6a127ca2f1749e
Poxxy <41162160+Poxxy@users.noreply.github.com>
2021-12-14 14:52:47 -0500

    New page, better docs, exception handling
    Added an info page which displays more content. Will be formatted in the future + display useful statistics. Cleaned up some code, added exception handling to db connection, and added documentation to certain functions.

--- flaskapp.py
+++ flaskapp.py
@@ -1,27 +1,38 @@
 from flask import Flask, render_template
 import psycopg2 
 
-def query_db():
-    conn = psycopg2.connect(
-                      host='yourhost',
-                      database='yourdb',
-                      user='youruser',
-                      password='yourpassword')
+def query_db(query):
+    """
+    Executes query. Careful not to open yourself up to SQL injection! 
+    You should either only give pre-defined queries or parameterize where input may occur.
+    """
+
+    try:
+        conn = psycopg2.connect(
+                        host='yourhost',
+                        database='yourdb',
+                        user='youruser',
+                        password='yourpassword')
+    except psycopg2.OperationalError as e:
+        issue = ('Unable to connect!\n{0}').format(e)
+        print(issue)
+        return issue
     
     cursor = conn.cursor()
 
-    query1 = """SELECT * FROM "Content" ORDER BY content_id DESC LIMIT 100;"""
+    cursor.execute(query)
 
-    cursor.execute(query1)
-
-    results1 = cursor.fetchall()
+    results = cursor.fetchall()
 
     cursor.close()
     conn.close()
 
-    return results1
+    return results
 
 def format_result(result):
+    """
+    Makes SELECT query for content table a bit prettier.
+    """
     text = ""
 
     for x in range(100):
@@ -33,15 +44,43 @@ def format_result(result):
     
     return text
 
+def update_tables():
+    users_update_query = """insert into "Users" (username)
+    select distinct username
+    from "Content"
+    where username not in (select username FROM "Users");
+    """
+    channels_update_query = """insert into "Channels" (channel) 
+    select distinct channel 
+    from "Content"
+    where channel not in (select channel FROM "Channels");
+    """
+
+    query_db(users_update_query)
+    query_db(channels_update_query)
 
 app = Flask(__name__)
 
 @app.route("/") #http://localhost:5000/
 def front():
-    info = query_db()
+    query = """SELECT * FROM "Content" ORDER BY content_id DESC LIMIT 100;"""
+    info = query_db(query)
     content = format_result(info)
 
     return content
+
+@app.route("/info")
+def info():
+    html = """<h1>Information Page</h1>
+    """
+    user_query = """select * from "Users" ORDER BY user_id DESC;"""
+
+    channel_query = """select * from "Channels" ORDER BY channel_id DESC;"""
+
+    users = query_db(user_query)
+    channels = query_db(channel_query)
+
+    return html + "<p>{}</p><p>{}</p>".format(users, channels)
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
